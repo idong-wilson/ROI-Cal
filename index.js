65b8f888fff4b81f39d23a763e21ff530100db3d
Idong Wilson <idongwilson@gmail.com>
2024-01-31 19:29:16 -0500

    added the calculation for AR

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ const data = {
     label: 'ROI Calculator',
     data: [invest.value, gain.value],
     backgroundColor: [
-      'rgba(255, 26, 104, 1)',
+      'rgba(166, 75, 28, 1.0)',
       'rgba(255, 206, 86, 1',
       
     ],
     borderColor: [
-      'rgba(255, 26, 104, 1)',
+      'rgba(166, 75, 28, 1.0)',
       'rgba(255, 206, 86, 1)',
       
     ],
@@ -58,4 +58,14 @@ function calculator() {
   const returns = document.getElementById('returns');
   const roi = (gain.value / invest.value) * 100;
   returns.innerHTML = roi.toFixed(2);
+
+  const proceeds = document.getElementById('proceeds');
+  const returnedAmount = parseInt(gain.value) + parseInt(invest.value);
+  proceeds.innerHTML = returnedAmount;
+
+  const annualized = document.getElementById('annualized');
+  const annualizedReturns = (returnedAmount / invest.value) ** (1 / parseFloat(duration.value)) - 1;
+  const annualizedReturnsPercentage = annualizedReturns * 100;
+  annualized.innerHTML = annualizedReturnsPercentage.toFixed(1);
+
 };
